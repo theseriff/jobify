2dcd6f2acaae6557bef6ed21713181f5328b72e6
Sergey <maclovi.dev@gmail.com>
2025-09-07 16:24:06 +0300

    update (#4)
    * refactor: func type_guards has been moved
    
    * chore(deps): bump the github-actions/pip
    
    Bumps the github-actions group with 2 updates:
    [actions/checkout](https://github.com/actions/checkout) and
    [actions/setup-python](https://github.com/actions/setup-python).
    
    Updates `actions/checkout` from 4 to 5
    - [Release notes](https://github.com/actions/checkout/releases)
    -
    [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/actions/checkout/compare/v4...v5)
    
    Updates `actions/setup-python` from 5 to 6
    - [Release notes](https://github.com/actions/setup-python/releases)
    - [Commits](https://github.com/actions/setup-python/compare/v5...v6)
    
    ---
    updated-dependencies:
    - dependency-name: actions/checkout
      dependency-version: '5'
      dependency-type: direct:production
      update-type: version-update:semver-major
      dependency-group: github-actions
    - dependency-name: actions/setup-python
      dependency-version: '6'
      dependency-type: direct:production
      update-type: version-update:semver-major
      dependency-group: github-actions
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ src/taskaio/_internal/_type_guards.py
@@ -0,0 +1,18 @@
+import asyncio
+from collections.abc import Callable, Coroutine
+from typing import ParamSpec, TypeGuard, TypeVar
+
+_P = ParamSpec("_P")
+_R = TypeVar("_R")
+
+
+def is_sync_callable(
+    func: Callable[_P, _R | Coroutine[None, None, _R]],
+) -> TypeGuard[Callable[_P, _R]]:
+    return not asyncio.iscoroutinefunction(func)
+
+
+def is_async_callable(
+    func: Callable[_P, _R | Coroutine[None, None, _R]],
+) -> TypeGuard[Callable[_P, Coroutine[None, None, _R]]]:
+    return asyncio.iscoroutinefunction(func)
