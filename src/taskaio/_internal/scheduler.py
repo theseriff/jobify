4ceb3e86e7cbbe3c0376a4ca299ceb0bddcbdc08
Maclovi <maclovi.dev@gmail.com>
2025-09-10 01:27:29 +0300

    breaking changes: some changes

--- src/taskaio/_internal/scheduler.py
+++ src/taskaio/_internal/scheduler.py
@@ -1,111 +1,55 @@
-import asyncio
-import functools
-import os
-import sys
-import warnings
-from collections.abc import Callable, Coroutine
-from typing import Any, ParamSpec, TypeVar, cast, overload
+from __future__ import annotations
 
-from taskaio._internal._type_guards import is_async_callable
+from typing import TYPE_CHECKING, Any, ParamSpec, TypeVar
+
 from taskaio._internal._types import EMPTY
-from taskaio._internal.exceptions import LambdaNotAllowedError
-from taskaio._internal.taskplan.async_task import TaskPlanAsync
-from taskaio._internal.taskplan.sync_task import TaskPlanSync
+from taskaio._internal.wrapper_func import FuncID, WrapperFunc
+
+if TYPE_CHECKING:
+    import asyncio
+    from collections.abc import Callable
+
+    from taskaio._internal.task_plan import TaskPlan
+
 
 _P = ParamSpec("_P")
 _R = TypeVar("_R")
 
 
-class TaskAIO:
+class TaskScheduler:
     __slots__: tuple[str, ...] = (
-        "_callback_registry",
+        "_func_registry",
         "_is_planning",
         "_loop",
         "_scheduled_tasks",
     )
 
     def __init__(self, loop: asyncio.AbstractEventLoop = EMPTY) -> None:
-        self._callback_registry: dict[str, Callable[..., Any]] = {}  # pyright: ignore[reportExplicitAny]
+        self._func_registry: dict[
+            FuncID,
+            WrapperFunc[..., Any],  # pyright: ignore[reportExplicitAny]
+        ] = {}
         self._is_planning: bool = False
         self._loop: asyncio.AbstractEventLoop = loop
-        self._scheduled_tasks: list[
-            TaskPlanSync[Any] | TaskPlanAsync[Any]  # pyright: ignore[reportExplicitAny]
-        ] = []
+        self._scheduled_tasks: list[TaskPlan[Any]] = []  # pyright: ignore[reportExplicitAny]
 
-    def register(self, callback: Callable[_P, _R]) -> Callable[_P, _R]:
-        self._register(callback)
-
-        @functools.wraps(callback)
-        def inner(*args: _P.args, **kwargs: _P.kwargs) -> _R:
-            return callback(*args, **kwargs)
-
-        return inner
-
-    def register_all(self, *callbacks: Callable[_P, _R]) -> None:
-        for callback in callbacks:
-            self._register(callback)
-
-    def _register(
+    def register(
         self,
         func: Callable[_P, _R],
         *,
-        callback_id: str | None = None,
-    ) -> None:
-        callback_id = callback_id or self._get_callback_id(func)
-        self._callback_registry[callback_id] = func
+        func_id: str | None = None,
+    ) -> WrapperFunc[_P, _R]:
+        wrapper_func = WrapperFunc(
+            origin_func=func,
+            scheduled_tasks=self._scheduled_tasks,
+            loop=self._loop,
+            func_id=func_id,
+        )
+        self._register(wrapper_func)
+        return wrapper_func
 
-    def _get_callback_id(self, func: Callable[_P, _R]) -> str:
-        fname = func.__name__
-        fmodule = func.__module__
-        if fname == "<lambda>":
-            raise LambdaNotAllowedError
-        if fmodule == "__main__":
-            fmodule = sys.argv[0].removesuffix(".py").replace(os.path.sep, ".")
-        return f"{fmodule}:{fname}"
-
-    @overload
-    def schedule(  # type: ignore[overload-overlap]
-        self,
-        func: Callable[_P, Coroutine[None, None, _R]],
-        *args: _P.args,
-        **kwargs: _P.kwargs,
-    ) -> TaskPlanAsync[_R]: ...
-
-    @overload
-    def schedule(
-        self,
-        func: Callable[_P, _R],
-        *args: _P.args,
-        **kwargs: _P.kwargs,
-    ) -> TaskPlanSync[_R]: ...
-
-    def schedule(
-        self,
-        func: Callable[_P, _R | Coroutine[None, None, _R]],
-        *args: _P.args,
-        **kwargs: _P.kwargs,
-    ) -> TaskPlanSync[_R] | TaskPlanAsync[_R]:
-        callback_id = self._get_callback_id(func)
-        if callback_id not in self._callback_registry:
-            self._register(func, callback_id=callback_id)
-            warnings.warn(
-                f"Function {func.__name__!r} from module {func.__module__!r} "
-                f"was not pre-registered. It has been automatically registered"
-                f" with ID: {callback_id}. For better control and explicit "
-                "registration, please use the register() method"
-                "before scheduling tasks.",
-                UserWarning,
-                stacklevel=2,
-            )
-
-        task_plan: TaskPlanAsync[_R] | TaskPlanSync[_R]
-        if is_async_callable(func):
-            task_plan = TaskPlanAsync(self._loop, func, *args, **kwargs)
-        else:
-            func = cast("Callable[_P, _R]", func)
-            task_plan = TaskPlanSync(self._loop, func, *args, **kwargs)
-        self._scheduled_tasks.append(task_plan)
-        return task_plan
+    def _register(self, func: WrapperFunc[_P, _R]) -> None:
+        self._func_registry[func.func_id] = func
 
     async def wait_for_complete(self) -> None:
         tasks = self._scheduled_tasks
---
+++ test.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from taskaio import TaskScheduler
+
+scheduler = TaskScheduler()
+
+
+@scheduler.register
+def f(a: int, b: str) -> None:
+    print(a, b)
+
+
+@scheduler.register
+async def f2(a: int, b: str) -> None:
+    print("from async def f2", a, b)
+
+
+f(1, "dsa")
+
+
+async def main() -> None:
+    f.schedule(2, "das").delay(1)
+    # f.schedule(3, "Dsa")
+    # await f2(21, "vvv")
+    # f2.schedule(4, "aaa").delay(1).to_thread()
+    # # f2.schedule(4, "das").delay(2)
+    await scheduler.wait_for_complete()
+
+
+asyncio.run(main())
