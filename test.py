63e300136403a01e3952550e8f7cbd8d3e5f89aa
Maclovi <maclovi.dev@gmail.com>
2025-09-24 18:32:09 +0300

    update

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import asyncio
 from datetime import datetime
+import itertools
+from typing import Literal
 
 from taskaio import TaskScheduler
 
@@ -13,9 +15,21 @@ def f(a: int, b: str) -> None:
 
 @scheduler.register(func_id="2")
 async def f2(a: int, b: str, c) -> None:
+    print("-" * 30)
     print("now:", datetime.now())
     print("tasks:", len(c))
     print("from async def f2: ----->", a, b)
+    print("-" * 30)
+
+
+@scheduler.register(func_id="2")
+async def f3(a: int, b: str, c) -> Literal[5]:
+    print("-" * 30)
+    print("now:", datetime.now())
+    print("tasks:", len(c))
+    print("from async def f3: ----->", a, b)
+    print("-" * 30)
+    return 5
 
 
 async def main() -> None:
@@ -24,11 +38,11 @@ async def main() -> None:
     # _ = f2(3, "4", scheduler._wrapper.task_registered).to_thread().delay(1)
     # await f2(2, "main", scheduler._wrapper.task_registered).call()
 
-    _ = f2(1, "cron", scheduler._wrapper.task_registered).cron("* * * * *")
+    _ = f2(2, "cron2", scheduler._wrapper.task_registered).cron("*/2 * * * *")
+    _ = f3(1, "cron3", scheduler._wrapper.task_registered).cron("* * * * *")
 
-    async for task_info in scheduler.wait_for_complete():
-        print(task_info)
-        task_info.cancel()
+    cnt = itertools.count()
+    await scheduler.wait_for_complete()
 
 
 asyncio.run(main())
